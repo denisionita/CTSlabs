4c54b94a67845acf77a4be4c81c63012fd33f29b
ionitadenis <ionitadenis3@yahoo.com>
2022-03-02 17:22:23 +0200

    UpperCase LowerCase

--- src/ro/ase/cts/gr1085/Main.java
+++ src/ro/ase/cts/gr1085/Main.java
@@ -2,16 +2,32 @@ package ro.ase.cts.gr1085;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Locale;
 
+class LowerCaseException extends Exception {
+    public LowerCaseException() {
+        System.out.println("litere mici");
+    }
+}
+class UpperCaseException extends Exception {
+    public UpperCaseException() {
+        System.out.println("litere mari");
+    }
+}
 public class Main {
-
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("tastati ceva");
+        System.out.println("tastati o propozitie care contine litere mari si mici:");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         try {
             String text = br.readLine();
+
+            //daca toate literele sunt mici (lower case) - LowerCaseException
+            //daca toate literele sunt mari (uppercase) - UpperCaseException
+            if(text.toLowerCase(Locale.ROOT).equals(text)) throw new LowerCaseException();
+            if(text.toUpperCase(Locale.ROOT).equals(text)) throw new UpperCaseException();
+
         }
         catch(Exception ex){
             System.out.println(ex.getMessage());
